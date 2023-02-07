3d6656b528ce10d84fdbafb089a42768f773175a
fedau <fedau.d@hotmail.com>
2023-02-07 10:40:11 +0000

    numberlist get size and add number

--- week_12/day2/Arrays/src/main/java/NumberList.java
+++ week_12/day2/Arrays/src/main/java/NumberList.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+
 public class NumberList {
 
     private ArrayList<Integer> numbers;
 
+
+    public NumberList(){
+        this.numbers = new ArrayList<>();
+    }
+
+    public int getNumberCount() {
+        return this.numbers.size();
+    }
+
+    public void addNumbers(int number) {
+        this.numbers.add(number);
+    }
 }
--- week_12/day2/Arrays/src/test/java/NumberListTest.java
+++ week_12/day2/Arrays/src/test/java/NumberListTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class NumberListTest {
+
+    private NumberList myNumbers;
+
+
+    @Before
+    public void before(){
+        myNumbers = new NumberList();
+    }
+
+    @Test
+    public void hasNumberofEntries(){
+        assertEquals(0, myNumbers.getNumberCount());
+    }
+
+    @Test
+    public void addNumberToList(){
+        myNumbers.addNumbers(12);
+        assertEquals(1, myNumbers.getNumberCount());
+    }
+
 }
