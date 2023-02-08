126c6040c9e713792a694dab8ba34aab1d688310
fedau <fedau.d@hotmail.com>
2023-02-08 10:20:46 +0000

    extending person hinheritance

---
+++ week_12/day3/inheritance_start_point/src/main/java/Instructor.java
@@ -0,0 +1,11 @@
+public class Instructor extends Person{
+
+    private String name;
+    private String cohort;
+
+    public Instructor(String name, String cohort) {
+        super(name, cohort);
+    }
+
+
+}
---
+++ week_12/day3/inheritance_start_point/src/main/java/Student.java
@@ -0,0 +1,10 @@
+public class Student extends Person {
+    private String name;
+    private String cohort;
+
+    public Student(String name, String cohort) {
+        super(name, cohort);
+    }
+
+
+}
