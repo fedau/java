3bdadfd0537cd2cae0a79026c804eba7ffa569c3
fedau <fedau.d@hotmail.com>
2023-02-08 10:14:45 +0000

    person methods

--- week_12/day3/inheritance_start_point/src/main/java/Person.java
+++ week_12/day3/inheritance_start_point/src/main/java/Person.java
@@ -1,2 +1,32 @@
 public class Person {
+
+    private String name;
+    private String cohort;
+    private String talk;
+
+    public Person(String name, String cohort){
+        this.name = name;
+        this.cohort = cohort;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getCohort() {
+        return this.cohort;
+    }
+
+    public void setName(String tony) {
+        this.name = tony;
+    }
+
+    public void setCohort(String e19) {
+        this.cohort = e19;
+    }
+
+    public String talk(String java) {
+        this.talk = java;
+        return "I love " + this.talk ;
+    }
 }
