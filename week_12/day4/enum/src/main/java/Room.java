7dd57c043bc651954295ef9e2af39f708ea331a5
fedau <fedau.d@hotmail.com>
2023-02-09 10:54:05 +0000

    enums

---
+++ week_12/day4/enum/src/main/java/Room.java
@@ -0,0 +1,21 @@
+public class Room {
+
+    private RoomType roomType;
+
+    public Room(RoomType roomType){
+       this.roomType = roomType;
+    }
+
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public int getValueFromEnum() {
+        return this.roomType.getValue();
+    }
+
+    public int getCapacity() {
+        return this.roomType.getCapacity();
+    }
+}
