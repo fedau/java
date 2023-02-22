48610d9bd1fddcfb28c93d0975bbef4f9fdc8ca5
fedau <fedau.d@hotmail.com>
2023-02-22 13:08:10 +0000

    derived queries for pirate

--- week_14/day_3/1_simple_derived_queries/start_point/src/main/java/com/example/codeclan/pirateservice/repository/PirateRepository.java
+++ week_14/day_3/1_simple_derived_queries/start_point/src/main/java/com/example/codeclan/pirateservice/repository/PirateRepository.java
@@ -4,6 +4,16 @@ import com.example.codeclan.pirateservice.models.Pirate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PirateRepository extends JpaRepository<Pirate, Long> {
+
+    long countByAgeGreaterThan(int age);
+    List<Pirate> findByAgeGreaterThan(int age);
+
+    List<Pirate> findByFirstName(String firstName);
+
+    List<Pirate> findDistinctByFirstNameAndLastName(String firstName, String lastName);
 }
+
