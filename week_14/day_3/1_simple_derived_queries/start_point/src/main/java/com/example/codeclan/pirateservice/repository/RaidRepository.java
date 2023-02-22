34375f45efb474bac90a28e9c7a5cd4ce18ed539
fedau <fedau.d@hotmail.com>
2023-02-22 13:08:21 +0000

    derived queries for raid

--- week_14/day_3/1_simple_derived_queries/start_point/src/main/java/com/example/codeclan/pirateservice/repository/RaidRepository.java
+++ week_14/day_3/1_simple_derived_queries/start_point/src/main/java/com/example/codeclan/pirateservice/repository/RaidRepository.java
@@ -3,5 +3,9 @@ package com.example.codeclan.pirateservice.repository;
 import com.example.codeclan.pirateservice.models.Raid;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RaidRepository extends JpaRepository<Raid, Long> {
+
+    List<Raid> findByLocation(String location);
 }
