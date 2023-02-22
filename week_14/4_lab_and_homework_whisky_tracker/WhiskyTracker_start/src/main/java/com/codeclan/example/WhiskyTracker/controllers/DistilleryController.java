923331b6c8623f4671bd587c324a396758a7f114
fedau <fedau.d@hotmail.com>
2023-02-22 15:15:05 +0000

    controllerrs

--- week_14/4_lab_and_homework_whisky_tracker/WhiskyTracker_start/src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ week_14/4_lab_and_homework_whisky_tracker/WhiskyTracker_start/src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -1,10 +1,34 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DistilleryController {
 
+@Autowired
+    DistilleryRepository distilleryRepository;
+
+    @GetMapping(value = "/distileries")
+    public ResponseEntity<List<Distillery>> getAlDistileries(){
+        return new ResponseEntity<>(distilleryRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/distileries/{id}")
+    public ResponseEntity getdistileries(@PathVariable Long id){
+        return new ResponseEntity<>(distilleryRepository.findById(id), HttpStatus.OK);
+    }
+    @PostMapping(value = "/distileries")
+    public ResponseEntity<Distillery> createdistileries(@RequestBody Distillery distillery){
+        distilleryRepository.save(distillery);
+        return new ResponseEntity<>(distillery, HttpStatus.CREATED);
+    }
 
 }
--- week_14/4_lab_and_homework_whisky_tracker/WhiskyTracker_start/src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ week_14/4_lab_and_homework_whisky_tracker/WhiskyTracker_start/src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,9 +1,32 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class WhiskyController {
 
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> getAllWhiskies(){
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/whiskies/{id}")
+    public ResponseEntity getwhiskies(@PathVariable Long id){
+        return new ResponseEntity<>(whiskyRepository.findById(id), HttpStatus.OK);
+    }
+    @PostMapping(value = "/whiskies")
+    public ResponseEntity<Whisky> createwhiskies(@RequestBody Whisky wisky){
+        whiskyRepository.save(wisky);
+        return new ResponseEntity<>(wisky, HttpStatus.CREATED);
+    }
 }
