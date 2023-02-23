cb966a3725a72d465104791b3954926712743b1d
fedau <fedau.d@hotmail.com>
2023-02-23 11:44:04 +0000

    fetch request and delete

--- week_14/day_4/full_stack/pirates_all_day_react_start/src/helpers/request.js
+++ week_14/day_4/full_stack/pirates_all_day_react_start/src/helpers/request.js
@@ -1,5 +1,17 @@
 class Request {
-    // TODO: methods to handle requests to API
+    //Fetch data from api
+    async get(url){
+        const result = await fetch(url); //setup a proxy in package json because this is a react url right now
+        return result.json()
+    }
+
+    delete(url){
+        return fetch(url, {
+            method: "DELETE",
+            headers: { "Content-Type": "application/json"}
+        })
+    }
+
 }
 
 export default Request;
